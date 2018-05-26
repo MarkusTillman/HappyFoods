6276ef25229292379b3bf94228336e6772804f68
Markus Tillman <markustillman91@gmail.com>
2018-05-26 18:58:20 +0200

    Add reading and parsing of resources
    Added createStreamReaderFromResourceFile()-method for resources, and
    createCsvParser() for parsing comma separated files. The parsing classes
    come from the new dependency on apache commons.

--- src/java/com/happyfoods/utilities/io/FileUtilities.java
+++ src/java/com/happyfoods/utilities/io/FileUtilities.java
@@ -2,7 +2,10 @@ package com.happyfoods.utilities.io;
 
 import com.google.common.base.Strings;
 import com.happyfoods.utilities.exception.ExceptionUtilities;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 
+import java.io.InputStreamReader;
 import java.util.Optional;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -11,18 +14,31 @@ import static java.util.logging.Logger.getLogger;
 
 public class FileUtilities {
 
-	private static final Logger logger = getLogger(FileUtilities.class.getSimpleName());
+	static final Logger logger = getLogger(FileUtilities.class.getSimpleName());
 	private static final String SYSTEM_TEMPORARY_DIRECTORY_PATTERN = "%t/";
-
-	public static Optional<FileHandler> createFileHandler(String logFileName) {
-		if (Strings.isNullOrEmpty(logFileName)) {
-			logger.warning("Cannot create file handler without a name");
-			return Optional.empty();
-		}
-		return ExceptionUtilities.uncheckThrowable(() -> new FileHandler(logFileName)); // TODO: mock when testing.
-	}
 
 	public static String getSystemTemporaryDirectoryPattern() {
 		return SYSTEM_TEMPORARY_DIRECTORY_PATTERN;
+	}
+
+	public static Optional<FileHandler> createFileHandler(String fileName) {
+		if (Strings.isNullOrEmpty(fileName)) {
+			logger.warning("Cannot create file handler without a name");
+			return Optional.empty();
+		}
+		return Optional.ofNullable(ExceptionUtilities.uncheckThrowable(() -> new FileHandler(fileName))); // TODO: mock when testing.
+	}
+
+	public static Optional<InputStreamReader> createStreamReaderFromResourceFile(String fileName) {
+		if (Strings.isNullOrEmpty(fileName)) {
+			logger.warning("Cannot create file reader without a name");
+			return Optional.empty();
+		}
+		return Optional.ofNullable(FileUtilities.class.getResourceAsStream("/" + fileName)).map(InputStreamReader::new);
+	}
+
+	public static Optional<CSVParser> createCsvParser(String fileName, CSVFormat csvFormat) {
+		Optional<InputStreamReader> fileReader = createStreamReaderFromResourceFile(fileName);
+		return fileReader.map(reader -> ExceptionUtilities.uncheckThrowable(() -> CSVParser.parse(fileReader.get(), csvFormat)));
 	}
 }
--- test/java/com/happyfoods/utilities/io/FileUtilitiesTest.java
+++ test/java/com/happyfoods/utilities/io/FileUtilitiesTest.java
@@ -3,7 +3,9 @@ package com.happyfoods.utilities.io;
 import com.happyfoods.framework.TestExtension;
 import com.happyfoods.utilities.annotation.TagFast;
 import com.happyfoods.utilities.reflection.ReflectionUtilities;
+import org.apache.commons.csv.CSVFormat;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -31,27 +33,74 @@ class FileUtilitiesTest {
 		ReflectionUtilities.setPrivateStaticFinalField(loggerField, logger);
 	}
 
-	@Test
-	void testThatFileHandlerWithNullNameReturnsEmptyAndLogsWarning() {
-		assertThat(FileUtilities.createFileHandler(null)).isEmpty();
-		verify(logger).warning("Cannot create file handler without a name");
+	@Nested
+	class Miscellaneous {
+		@Test
+		void testThatSystemTemporaryDirectoryPatternIsPercentageAndTheLetterTEscapedWithSlash() {
+			assertThat(FileUtilities.getSystemTemporaryDirectoryPattern()).isEqualTo("%t/");
+		}
 	}
 
-	@Test
-	void testThatFileHandlerWithEmptyNameReturnsEmptyAndLogsWarning() {
-		assertThat(FileUtilities.createFileHandler("")).isEmpty();
-		verify(logger).warning("Cannot create file handler without a name");
+	@Nested
+	class FileHandlerTests {
+		@Test
+		void testThatCreatingHandlerWithNullNameReturnsEmptyAndLogsWarning() {
+			assertThat(FileUtilities.createFileHandler(null)).isEmpty();
+			verify(logger).warning("Cannot create file handler without a name");
+		}
+
+		@Test
+		void testThatCreatingHandlerWithEmptyNameReturnsEmptyAndLogsWarning() {
+			assertThat(FileUtilities.createFileHandler("")).isEmpty();
+			verify(logger).warning("Cannot create file handler without a name");
+		}
+
+		@Test
+		void testThatHandlerWithNameCanBeCreated() {
+			FileHandler handler = FileUtilities.createFileHandler("fileHandler").get();
+			verifyZeroInteractions(logger);
+			handler.close(); // todo: remove once test framework deals with this
+		}
 	}
 
-	@Test
-	void testThatFileHandlerWithNameCanBeCreated() {
-		FileHandler handler = FileUtilities.createFileHandler("fileHandler").get();
-		verifyZeroInteractions(logger);
-		handler.close(); // todo: remove once test framework deals with this
+	@Nested
+	class InputStreamReaderTests {
+		@Test
+		void testThatCreatingReaderWithNullNameReturnsEmptyAndLogsWarning() {
+			assertThat(FileUtilities.createStreamReaderFromResourceFile(null)).isEmpty();
+			verify(logger).warning("Cannot create file reader without a name");
+		}
+
+		@Test
+		void testThatCreatingReaderWithEmptyNameReturnsEmptyAndLogsWarning() {
+			assertThat(FileUtilities.createStreamReaderFromResourceFile("")).isEmpty();
+			verify(logger).warning("Cannot create file reader without a name");
+		}
+
+		@Test
+		void testThatCreatingReaderFromANonResourceFileReturnsEmptyAndDoesNotLog() {
+			assertThat(FileUtilities.createStreamReaderFromResourceFile("NonExisting.resource")).isEmpty();
+			verifyZeroInteractions(logger);
+		}
+
+		@Test
+		void testThatReaderWithNameCanBeCreatedFromResourceFile() {
+			FileUtilities.createStreamReaderFromResourceFile("Nutrients.csv").get();
+			verifyZeroInteractions(logger);
+		}
 	}
 
-	@Test
-	void testThatSystemTemporaryDirectoryPatternIsPercentageAndTheLetterTEscapedWithSlash() {
-		assertThat(FileUtilities.getSystemTemporaryDirectoryPattern()).isEqualTo("%t/");
+	@Nested
+	class CSVTests {
+		@Test
+		void testThatCreatingACsvParserUsingFormatDifferentFromFileReturnsOptionalWithValue() {
+			assertThat(FileUtilities.createCsvParser("Nutrients.csv", CSVFormat.TDF)).isNotEmpty();
+		}
+
+		@Test
+		void testThatCreatingACsvParserUsingFormatSameAsFileReturnsOptionalWithValue() {
+			CSVFormat csvFormat = CSVFormat.newFormat(',').withCommentMarker('#');
+			assertThat(FileUtilities.createCsvParser("Nutrients.csv", csvFormat)).isNotEmpty();
+		}
 	}
 }
